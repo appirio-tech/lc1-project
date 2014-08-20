d0f2f38373b283c26178b7fe58b03c295311906f
Neil Hastings <neil.hastings@gmail.com>
2014-08-20 08:45:14 -0400

    Reformat challenge model.

--- packages/challenges/server/models/challenge.js
+++ packages/challenges/server/models/challenge.js
@@ -10,39 +10,39 @@ var sequelize = postgresql.sequelize;
 
 
 sequelize.define('Challenge', {
-	startDate: Sequelize.DATE,
-	endDate: Sequelize.DATE,
-	title: {type: Sequelize.TEXT, allowNull: false, unique: true},
-	overview: Sequelize.TEXT,	// rich text
-	description: Sequelize.STRING(140),
-	functionalRequirements: {
-		type: Sequelize.JSON, 	// JSON
-		set: function(value) {
-			return this.setDataValue('functionalRequirements', JSON.stringify(value));
-		}
-	},
-	technicalRequirements: {
-		type: Sequelize.JSON, 	// JSONB
-		set: function(value) {
-			return this.setDataValue('technicalRequirements', JSON.stringify(value));
-		//},
-		// JSONB type return string, so it requires JSON.parse.
-		//get: function() {
-		//	return JSON.parse(this.getDataValue('technicalRequirements'));
-		}
-	},
-	tags: Sequelize.ARRAY(Sequelize.TEXT)	// Array of Text
+    startDate: Sequelize.DATE,
+    endDate: Sequelize.DATE,
+    title: {type: Sequelize.TEXT, allowNull: false, unique: true},
+    overview: Sequelize.TEXT,	// rich text
+    description: Sequelize.STRING(140),
+    functionalRequirements: {
+      type: Sequelize.JSON, 	// JSON
+      set: function (value) {
+        return this.setDataValue('functionalRequirements', JSON.stringify(value));
+      }
+    },
+    technicalRequirements: {
+      type: Sequelize.JSON, 	// JSONB
+      set: function (value) {
+        return this.setDataValue('technicalRequirements', JSON.stringify(value));
+        //},
+        // JSONB type return string, so it requires JSON.parse.
+        //get: function() {
+        //	return JSON.parse(this.getDataValue('technicalRequirements'));
+      }
+    },
+    tags: Sequelize.ARRAY(Sequelize.TEXT)	// Array of Text
 
-},
-{tableName: 'challenges'});
+  },
+  {tableName: 'challenges'});
 
 // sync with database
 sequelize
-	.sync({force: false})
-	.complete(function(err) {
-		if (!!err) {
-			console.log('An error occurred while syncing database:', err);
-		} else {
-			console.log('Database is synced!');
-		}
-	});
+  .sync({force: false})
+  .complete(function (err) {
+    if (!!err) {
+      console.log('An error occurred while syncing database:', err);
+    } else {
+      console.log('Database is synced!');
+    }
+  });

c3a6949816ea6f12852ab68d849c48746c947ea3
Ritesh Sangwan <sangwan.ritesh@yahoo.in>
2014-09-11 01:44:49 +0530

    file upload configuration added

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,43 @@
 'use strict';
 
 module.exports = {
+
+  /**
+   * Uploads configuration
+   * If local storage will be false then application by defaults initialize S3 upload service
+   * For local storage uploadsDirectory is mandatory.
+   * If temp directory is not given it will default to os.tempdir()
+   *
+   * For s3 upload configuration
+   * key, secret bucket and region properties are mandatory
+   * @type {Object}
+   */
+  uploads : {
+    isLocalStorage : true,
+    uploadsDirectory : './uploads',
+    tempDir : './temp',
+    /**
+     * Constant representing Local storage type
+     * @type {Number}
+     */
+    localStorage : 1,
+    /**
+     * Constant representing S3 Storage type
+     * @type {Number}
+     */
+    s3Storage : 2
+  },
+  /**
+   * AWS configuration for s3 upload service
+   * @type {Object}
+   */
+  aws : {
+    secure: false,
+    key: 'AKIAJZS2NZA4O6QGYWJA',
+    secret: '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',
+    bucket: 'challenge-api',
+    region : 'ap-southeast-1'
+  },
   db: 'mongodb://heroku_app28672932:lbjk306k1jr7r13g25sbj3a1rr@ds063809.mongolab.com:63809/heroku_app28672932',
   app: {
     name: '[topcoder serenity]'
@@ -38,12 +75,8 @@ module.exports = {
       pass: 'PASSWORD'
     }
   },
+  postgresurl : 'postgres://idgimrogqewzfg:-C6oh1ld9u_pm4201sctLVqPwX@ec2-54-197-250-52.compute-1.amazonaws.com:5432/dc6c3p1lnfqrvo',
   pg: {
-    database: 'dc6c3p1lnfqrvo',
-    username: 'idgimrogqewzfg',
-    password: '-C6oh1ld9u_pm4201sctLVqPwX',
-    host: 'ec2-54-197-250-52.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    port: 5432
+    dialect: 'postgres'
   }
 };

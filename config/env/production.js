cd7d7daf9d46e19bc77208b0537949972d83dd44
Ritesh Sangwan <sangwan.ritesh@yahoo.in>
2014-09-13 02:18:16 +0530

    storage provider configuration added

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = {
+  /**
+   * Uploads configuration
+   * @type {Object}
+   */
+  uploads : {
+    /**
+     * Should be configured in storageProviders
+     * @type {String}
+     */
+    storageProvider : 'local'
+  },
   db: process.env.MONGOLAB_URI,
   app: {
     name: '[topcoder serenity]'
@@ -40,5 +51,55 @@ module.exports = {
   },
   pg: {
     // Not needed in production.  Code reads from DATABASE_URL env variable
+  },
+  /**
+   * Storage providers can be configured here
+   * A storage provider should support two operations
+   * store and delete
+   * @type {Object}
+   */
+  storageProviders : {
+    local : {
+      /**
+       * This path is needed to load the provider during application load
+       * @type {String}
+       */
+      path : './packages/challenges/server/middleware/LocalUploadMiddleware',
+      options : {
+        /**
+         * Unique Id for this storage provider
+         * NOTE: Every storage provider should have a unique id
+         * @type {Number}
+         */
+        id : 1,
+        /**
+         * These are upload directories for local storage provider
+         * @type {String}
+         */
+        uploadsDirectory : './uploads',
+        tempDir : './temp'
+      }
+    },
+    amazonS3 : {
+      path : './packages/challenges/server/middleware/S3UploadMiddleware',
+      options : {
+        /**
+         * This path is needed to load the provider during application load
+         * @type {String}
+         */
+        id: 2,
+        /**
+         * AWS configuration for s3 upload service
+         * @type {Object}
+         */
+        aws : {
+          secure: false,
+          key: 'KEY',
+          secret: 'SECRET_KEY',
+          bucket: 'bucket',
+          region : 'region'
+        }
+      }
+    }
   }
 };
